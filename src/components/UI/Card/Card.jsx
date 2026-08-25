e0aae9c41db46b23c55d7db596c4cc18d99cbcfb
shaza-alkheder <shazakheder812@gmail.com>
2026-08-25 18:39:07 +0300

    create Card component

--- src/components/UI/Card/Card.jsx
+++ src/components/UI/Card/Card.jsx
@@ -1,22 +1,49 @@
 import "./Card.css";
-const Card = ({ image,icon, title, desc,classNames = {} }) => {
+const Card = ({ image, icon, imageTitle, title, desc, classNames = {} }) => {
   return (
     <>
       <div className={`skCard ${classNames.card || ""}`}>
         {image ? (
-        <div className={` ${classNames.imageDiv || ""}`}>
-          <img src={image} alt={title || ""}  className={` ${classNames.image || ""}`}/>
+          <div className={` ${classNames.imageDiv || ""}`}>
+            <img
+              src={image}
+              alt={title || ""}
+              className={` ${classNames.image || ""}`}
+            />
+          </div>
+        ) : icon ? (
+          <div className={`iconCard ${classNames.icon || ""}`}>{icon}</div>
+        ) : null}
+        {title ? (
+        <div className={classNames.imgTitleDiv || ""}>
+          {title ? <h2 className={classNames.title || ""}>{title}</h2> : null}
+          {imageTitle ? (
+            <img
+              src={imageTitle}
+              alt=""
+              className={` ${classNames.imgTitle || ""}`}
+            />
+          ) : null}
         </div>
-      ) : icon ? (
-        <div className={`iconCard ${classNames.icon || ""}`}>
-          {icon}
-        </div>
-      ) : null}
-        {title ? <h2 className={classNames.title || ""}>{title}</h2> : null}
-        {desc ?   <p className={classNames.desc || ""}>{desc}</p> : null}
+        ):null}
+
+        {desc ? <p className={classNames.desc || ""}>{desc}</p> : null}
       </div>
     </>
   );
 };
 
 export default Card;
+
+
+
+// example
+    // <Card
+    //     image="/img/Icon.png"
+    //     title="Loans and Mortgages"
+    //     desc="Realize your dreams with our flexible loan and mortgage options."
+    //     classNames={{
+    //       card: "securityCard",
+    //       title: "securityCardTitle",
+    //     }}
+    //   /> 
