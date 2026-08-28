d65b618eb4fb04b75c438fc9f9210bbe8ba4e60b
shaza-alkheder <shazakheder812@gmail.com>
2026-08-28 22:32:48 +0300

    create Benefits component

---
+++ src/components/Sections/Benefits/Benefits.jsx
@@ -0,0 +1,59 @@
+import { useState } from 'react';
+import TitleDescription from '../../UI/TitleDescription/TitleDescription'
+import './Benefits.css'
+import BenefitsCardData from "../../../data/BenefitsCardData.json";
+import Card from "../../UI/Card/Card";
+
+
+const Benefits = () => {
+    const [benefitsContent] = useState(() => {
+    const benefitsStored = localStorage.getItem("benefitsContent");
+
+    if (benefitsStored) {
+      return JSON.parse(benefitsStored);
+    }
+
+    localStorage.setItem("benefitsContent", JSON.stringify(BenefitsCardData));
+    return BenefitsCardData;
+  });
+  return (
+    <>
+      
+      <section className="S-K-Benefits">
+         <TitleDescription
+          titleParts={[
+            {
+              text: "Our",
+              className: "",
+            },
+            {
+              text: "Benefits",
+              className: "DS_ColorLimeGreen",
+            },
+          ]}
+          description="At YourBank, we value our employees and are dedicated to their well-being and success. We offer a comprehensive range of benefits designed to support their personal and professional growth."
+        />
+        <div className='S-K-BenefitsCard'>
+   {benefitsContent.map((data) => (
+            <Card
+              key={data.id}
+              title={data.title}
+              titleIcon={<div className="borderImage"><img src={data.image} alt={data.title} className="iconImage"/></div>}
+              desc={data.desc}
+              classNames={{
+                imgTitleDiv: "S-K-headerCardBenefits",
+                card: "S-K-cardBenefitsStyle",
+                title: "S-K-titleCardBenefits",
+                titleIcon: "S-K-titleIcon",
+                desc: "S-K-descriptionCardBenefits",
+              }}
+            />
+          ))}
+        </div>
+      </section>
+       
+    </>
+  )
+}
+
+export default Benefits
