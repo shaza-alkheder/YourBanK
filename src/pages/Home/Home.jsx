dac45f970fbb659e80b97f639679d86fccfd79b4
Marie Ghanem <ghanemmarie524@gmail.com>
2026-08-29 17:20:55 +0300

    add hero

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,15 +2,26 @@
 import "./Home.css"
 import OurFeatures from "../../components/Sections/OurFeatures/OurFeatures"
 import Container from "../../components/UI/container/container"
+import Hero from "../../components/Layout/Hero/Hero"
 
 
 
 function Home() {
   return (
-    <div>
+    <div className="home">
+      <img src="/assets/img/HomePage/Objects.webp" 
+      alt="" 
+      className="MGobjects"/>
       <main>
         <Container>
+          <Hero iconHero="/assets/img/HomePage/iconHero.png"
+                arrowsHero="/assets/img/HomePage/arrowsHero.webp"
+                imageHero="/assets/img/HomePage/imageHero.webp"
+          
+          
+          />
           <OurFeatures /> 
+        
         </Container>
       </main>
     </div>  
