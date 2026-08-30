9d67ac4bb5e41a2e04fc76f160657ecda8e38a37
Dalaa suleiman <dalaasoleman@7gmail.com>
2026-08-30 20:57:48 +0300

    test

---
+++ src/components/UI/CardJobOpenings/CardJobOpenings.jsx
@@ -0,0 +1,35 @@
+import Button from '../Button/Button'
+import './CardJobOpenings.css'
+import { FaBriefcase } from "react-icons/fa6";
+
+const CardJobOpenings = ({job}) => {
+    return (
+        <div className='DS_CardJobOpenings'>
+            <h2 className="DS_jobTitle">{job.title}</h2>
+            <div className="DS_jobTags">
+                <span className="DS_jobTag">Location: {job.location}</span>
+                <span className="DS_jobTag">Department: {job.department}</span>
+            </div>
+
+        <h3 className='DS_TitleAbout'>About This Job</h3>
+        <p className='DS_DescAbout'>{job.about}</p>
+
+        <h3 className='DS_TitleRequire'>Requirements & Qualifications</h3>
+        <ul className="DS_jobRequirementsList">
+            {job.requirements.map((req, index) => (
+                <li key={index} className="DS_jobReqItem">
+                    <FaBriefcase />
+                    <span className='DS_ReqText'>{req}</span>
+                </li>
+            ))}
+        </ul>
+
+        <Button 
+        className="DS_styleTest"
+        content ="Apply Now"
+        /> 
+        </div>
+    )
+    }
+
+export default CardJobOpenings
--- src/pages/Careers/Careers.jsx
+++ src/pages/Careers/Careers.jsx
@@ -1,5 +1,6 @@
 import Benefits from "../../components/Sections/Benefits/Benefits"
 import FAQ from "../../components/Sections/FAQ/FAQ"
+import JobOpenings from "../../components/Sections/JobOpenings/JobOpenings"
 import Container from "../../components/UI/Container/Container"
 
 
