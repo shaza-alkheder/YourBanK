2b2c117be6efd7933c3c705846b4ace329e047e8
shaza-alkheder <shazakheder812@gmail.com>
2026-08-30 19:33:50 +0300

    create CAT section

--- src/components/Sections/CTA/CTA.jsx
+++ src/components/Sections/CTA/CTA.jsx
@@ -5,6 +5,7 @@ const CTA = () => {
   return (
     <>
       <section className='S-K-CTA'>
+        <img src='/assets/img/HomePage/AbstractDesign.webp' alt='' className='imgdesign'/>
   <div className='S-K-contentCTA'>
     <h2>Start your financial journey with <span>YourBank today!</span></h2>
     <p>Ready to take control of your finances? Join YourBank now, and let us help you achieve your financial goals with our tailored solutions and exceptional customer service</p>
