560f1434f0132a8c67ea3bfa3d3617ab00defaea
Omar-Alshegry <omar.shegry.1999@gmail.com>
2026-09-06 20:12:05 +0300

    table dashboard

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,10 +1,18 @@
 import Sidebar from '../../components/Dashboard/Layout/Sidebar/Sidebar'
+import TableDashboard from '../../components/Dashboard/Layout/TableDashboard/TableDashboard'
+import ContaninerDashboard from '../../components/Dashboard/UI/ContaninerDashboard/ContaninerDashboard'
 import './Dashboard.css'
 
 const Dashboard  = () => {
   return (
     <>
       <Sidebar />
+      <ContaninerDashboard >
+        <TableDashboard 
+          title1 = "our"
+          title2 = "Feature"
+        />
+      </ContaninerDashboard>
     </>
   )
 }
