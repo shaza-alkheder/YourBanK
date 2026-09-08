7108200a3537b52459e674455ba57a661a3c9ee3
shaza-alkheder <shazakheder812@gmail.com>
2026-09-08 15:48:49 +0300

    update hero section

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,6 +1,5 @@
 import Sidebar from '../../components/Dashboard/Layout/Sidebar/Sidebar'
 import TableDashboard from '../../components/Dashboard/Layout/TableDashboard/TableDashboard'
-import FAQDashboard from '../../components/Dashboard/Sections/FAQDashboard/FAQDashboard'
 import ContaninerDashboard from '../../components/Dashboard/UI/ContaninerDashboard/ContaninerDashboard'
 import './Dashboard.css'
 
@@ -8,14 +7,12 @@ const Dashboard  = () => {
   return (
     <>
       <Sidebar />
-      {/* <ContaninerDashboard >
+      <ContaninerDashboard >
         <TableDashboard 
           title1 = "our"
           title2 = "Feature"
         />
-      </ContaninerDashboard> */}
-<FAQDashboard/>
-
+      </ContaninerDashboard>
     </>
   )
 }
