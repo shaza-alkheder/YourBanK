62330ec5aee97dd8c80eca90607b3c945f8e0bba
shaza-alkheder <shazakheder812@gmail.com>
2026-09-09 01:55:49 +0300

    create FAQDashboard section

--- src/components/Dashboard/UI/ModalDashboard/ModalDashboard.jsx
+++ src/components/Dashboard/UI/ModalDashboard/ModalDashboard.jsx
@@ -4,10 +4,12 @@ import FormDashboard from "../FormDashboard/FormDashboard";
 
 const ModalDashboard = ({
   title = "",
+  mode = "form",
   fields = [],
   data = {},
   onSubmit,
   onClose,
+  onDelete,
 }) => {
   return (
     <>
@@ -23,7 +25,38 @@ const ModalDashboard = ({
           <div className="S-K-Dashboard-Modal-header">
             <h2>{title}</h2>
           </div>
+              {mode === "delete" ? (
+          <div className="S-K-Dashboard-Modal-delete">
+
+            <p>
+              Are you sure you want to delete this FAQ?
+            </p>
+
+            <div className="S-K-Dashboard-Modal-delete-buttons">
+
+              <button
+                type="button"
+                className="S-K-Dashboard-Modal-cancel"
+                onClick={onClose}
+              >
+                Cancel
+              </button>
+
+              <button
+                type="button"
+                className="S-K-Dashboard-Modal-delete-btn"
+                onClick={onDelete}
+              >
+                Delete
+              </button>
+
+            </div>
+
+          </div>
+        ) : (
           <FormDashboard fields={fields} data={data} onSubmit={onSubmit} />
+            )}
+
         </div>
       </div>
     </>
