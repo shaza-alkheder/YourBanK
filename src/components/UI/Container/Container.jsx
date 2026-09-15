1f0750de3ddcd0fd0f13c8f819d4596a2aefc6a6
shaza-alkheder <shazakheder812@gmail.com>
2026-09-15 15:14:54 +0300

    Adding animation to the FAQ section

--- src/components/UI/Container/Container.jsx
+++ src/components/UI/Container/Container.jsx
@@ -1,6 +1,31 @@
 import "./Container.css"
-
-const Container = ({children}) => {
+import { motion } from "motion/react";
+const Container = ({children, animateOnScroll = false}) => {
+      if (animateOnScroll) {
+    return (
+      <motion.div
+        className="O-A-container"
+        initial={{
+          opacity: 0,
+          y: 50,
+        }}
+        whileInView={{
+          opacity: 1,
+          y: 0,
+        }}
+        viewport={{
+          once: true,
+          amount: 0.15,
+        }}
+        transition={{
+          duration: 1.6,
+          ease: [0.22, 1, 0.36, 1],
+        }}
+      >
+        {children}
+      </motion.div>
+    );
+  }
     return (
         <div className="O-A-container">
             {children}
